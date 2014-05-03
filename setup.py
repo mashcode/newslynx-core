df579acdfdb38469db8324c0ea779975b061d5f6
abelsonlive <brianabelson@gmail.com>
2014-05-03 12:04:32 -0400

    basic directory structure

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = [ 
+  str(ir.req) for ir in 
+  parse_requirements('requirements.txt')
+  ] 
+
+setup(
+  name='newslynx_code',
+  version='0.0.1',
+  description="",
+  long_description="",
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    ],
+  keywords='',
+  author='Brian Abelson, Stijn Debrouwere, Michael Keller',
+  author_email='brian@newslynx.org, stijn@newslynx.org, michael@newslynx.org',
+  url='http://github.com/newslynx/newslynx_core',
+  license='MIT',
+  packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+  namespace_packages=[],
+  include_package_data=False,
+  zip_safe=False,
+  install_requires=install_reqs,
+  tests_require=[],
+  entry_points={
+  }
+)
